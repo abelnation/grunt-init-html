96ba2b32fcc552bcea62862f7be8342be4c7bfd8
aallison <aallison@yahoo-inc.com>
2013-10-20 16:14:41 -0700

    more robust html project. js concat, sass compilation, prod/dev environments

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -1,37 +1,183 @@
 'use strict';
-var path = require('path');
-var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
-
-var folderMount = function folderMount(connect, point) {
-  return connect.static(path.resolve(point));
-};
 
 module.exports = function (grunt) {
   // Project configuration.
   grunt.initConfig({
-    connect: {
-      livereload: {
-        options: {
-          port: 9002,
-          middleware: function(connect, options) {
-            return [lrSnippet, folderMount(connect, '.')]
+    
+    "name": "my-project-name",
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    env : {
+      options : {
+          /* Shared Options Hash */
+          //globalOption : 'foo'
+      },
+      dev: {
+          NODE_ENV : 'DEVELOPMENT',
+          DEST: "build/dev"
+      },
+      prod : {
+          NODE_ENV : 'PRODUCTION',
+          DEST: "build/prod"
+      }
+    },
+    
+    preprocess : {
+      dev : {
+        src : './index.html',
+        dest : '<%= env.dev.DEST %>/' + 'index.html'
+      },
+      prod : {
+        src : './index.html',
+        dest : '<%= env.prod.DEST %>/' + 'index.html',
+        options : {
+          context : {
+            name : '<%= pkg.name %>',
+            version : '<%= pkg.version %>',
+            now : '<%= now %>',
+            ver : '<%= ver %>'
           }
         }
       }
     },
-    // Configuration to be run (and then tested)
-    regarde: {
-      project: {
-        files: ['*.js', '*.css', '*.html'],
-        tasks: ['livereload']
+
+    clean: {
+      files: ['css/{%= name %}.css', 'js/<%= pkg.name %>.min.js', 'js/<%= pkg.name %>.js', 'build/**']
+    },
+
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'compressed',
+          compass: true,
+        },
+        files: {                         // Dictionary of files
+          'css/{%= name %}.min.css': 'scss/{%= name %}.scss',       // 'destination': 'source'
+        }
+      },
+      dev: {
+        options: {                       // Target options
+          style: 'expanded',
+          compass: true,
+          lineNumbers: true,
+        },
+        files: {                         // Dictionary of files
+          'css/{%= name %}.css': 'scss/{%= name %}.scss',       // 'destination': 'source'
+        }
       }
-    }
+    },
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true,
+        separator: ';'
+      },
+      dist: {
+        src: ['js/src/**/*.js'],
+        dest: 'js/<%= pkg.name %>.js'
+      },
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'js/<%= pkg.name %>.min.js'
+      },
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    jshint: {
+      gruntfile: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        src: 'Gruntfile.js'
+      },
+      src: {
+        options: {
+          jshintrc: 'js/.jshintrc'
+        },
+        src: ['js/src/**/*.js']
+      },
+      test: {
+        options: {
+          jshintrc: 'test/.jshintrc'
+        },
+        src: ['test/**/*.js']
+      },
+    },
+
+    copy: {
+      prod:  {
+        files: [
+          // Copy require.js file from dev/libs/require/ to prod/scripts/.
+          { expand: true,  src: ['js/lib/**'], dest: '<%= env.prod.DEST %>' } ,
+          { expand: true,  src: ['<%= uglify.dist.dest %>'], dest: '<%= env.prod.DEST %>/' } ,
+          // { expand: true,  src: ['*.html'], dest: '<%= env.prod.DEST %>' } ,
+          { expand: true,  src: ['css/**'], dest: '<%= env.prod.DEST %>' } ,
+          { expand: true, src: ['img/**'], dest: '<%= env.dev.DEST %>' } ,
+        ]
+      },
+      dev: {
+        files: [
+          { expand: true,  src: ['js/lib/**'], dest: '<%= env.dev.DEST %>' } ,
+          { expand: true,  src: ['js/src/**'], dest: '<%= env.dev.DEST %>' } ,
+          // { expand: true,  src: ['*.html'], dest: '<%= env.dev.DEST %>' } ,
+          { expand: true,  src: ['css/**'], dest: '<%= env.dev.DEST %>' } ,
+          { expand: true, src: ['img/**'], dest: '<%= env.dev.DEST %>' } ,
+        ]
+      }
+    },
+
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      js: {
+        files: '<%= jshint.src.src %>',
+        tasks: ['jshint:src', 'qunit']
+      },
+      sass: {
+        files: '<%= sass.dist.files %>',
+        tasks: ['sass']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'qunit']
+      },
+      scripts: {
+        files: ['**/*.js'],
+        tasks: ['jshint'],
+        options: {
+          spawn: false,
+        },
+      },
+
+    },
 
   });
 
-  grunt.loadNpmTasks('grunt-regarde');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-livereload');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-preprocess');
+  grunt.loadNpmTasks('grunt-env');
 
-  grunt.registerTask('default', ['livereload-start', 'connect', 'regarde']);
+  grunt.registerTask('default', ['dev']);
+  grunt.registerTask('dev', ['env:dev', 'preprocess:dev', 'jshint', 'sass', 'copy:dev', 'qunit']);
+  grunt.registerTask('prod', ['env:prod', 'preprocess:prod', 'jshint', 'concat', 'uglify', 'copy:prod', 'sass']);
 };
--- template.js
+++ template.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.description = 'Create a basic html/css/js project.';
+exports.description = 'Create a basic html/sass/css/js project.';
 
 exports.notes = 'yeah, html!';
 
@@ -24,24 +24,37 @@ exports.template = function(grunt, init, done){
     init.prompt('version', '0.0.0'),
     init.prompt('node_version', '>= 0.10.0'),
   ], function(err, props){
+    // A few additional properties.
+    
     props.keywords = [];
-    props.devDependencies = {
-      'grunt': '~0.4.1',
-      'grunt-contrib-qunit': '~0.2.0',
-      'grunt-contrib-jshint': '~0.1.1',
-      'grunt-contrib-connect': '~0.1.2',
-      'grunt-contrib-livereload': '~0.1.2',
-      'grunt-regarde': '~0.1.1'
-    };
 
+    // Files to copy (and process).
     var files = init.filesToCopy(props);
 
+    // Add properly-named license files.
     init.addLicenseFiles(files, props.licenses);
 
-    init.copyAndProcess(files, props);
+    // Actually copy (and process) files.
+    init.copyAndProcess(files, props, {noProcess: 'js/lib/**'});
 
+    props.npm_test = 'grunt qunit';
+    props.devDependencies = {
+      'grunt-contrib-sass': '~0.5.0',
+      'grunt-contrib-jshint': '~0.6.0',
+      'grunt-contrib-qunit': '~0.2.0',
+      'grunt-contrib-concat': '~0.3.0',
+      'grunt-contrib-uglify': '~0.2.0',
+      'grunt-contrib-watch': '~0.4.0',
+      "grunt-contrib-copy": "~0.4.1",
+      "grunt-contrib-clean": "~0.5.0",
+      'grunt-preprocess': '~3.0.0',
+      'grunt-env': '~0.4.0',
+    };
+    // Generate package.json file, used by npm and grunt.
     init.writePackageJSON('package.json', props);
 
+    // All done!
     done();
+
   });
 };
